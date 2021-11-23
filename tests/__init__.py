9bd22935469f4b8f4dfed31b3f0d3ad4c47fa2f2
Irina <irina.digiovanni@intive.com>
2021-11-23 13:05:32 -0300

    Elimina base de test si existe

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 import os
 os.environ["DATABASE_ENV"] = "sqlite:////tmp/benancio_ventas_api.db"
+if os.path.exists("/tmp/benancio_ventas_api.db"):
+    os.remove("/tmp/benancio_ventas_api.db")
 
 from src.modelos import Base
 from conexion import conexion
