e0df74b149d75fb61f606aeec8245af51104355d
matias <maturod96@gmail.com>
2021-11-30 12:27:46 -0300

    Agrega variable de entorno para clave JWT

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
 
+
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Benancio Ventas API', description='La API Rest de Benancio Ventas')
-app.config['JWT_SECRET_KEY'] = 'Esta es mi clabe secreta'
+app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 app.config["ERROR_INCLUDE_MESSAGE"] = False
 jwt = JWTManager(app)
