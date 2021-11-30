0df39a751689ea945d47f094a3e9261d343deaff
matias <maturod96@gmail.com>
2021-11-30 12:43:21 -0300

    Setea variable de expiracion de token. 1 hora

--- src/__init__.py
+++ src/__init__.py
@@ -8,5 +8,6 @@ from flask_jwt_extended import JWTManager
 app = Flask(__name__)
 api = Api(app, version='1.0', title='Benancio Ventas API', description='La API Rest de Benancio Ventas')
 app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600
 app.config["ERROR_INCLUDE_MESSAGE"] = False
 jwt = JWTManager(app)
