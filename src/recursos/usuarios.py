b65958e696f406a1da3b3851a47ba1e894ed58db
Joaquin Santos <joaco.santos@gmail.com>
2021-12-06 16:03:59 -0300

    Agrega documentacion endpoint users

--- src/recursos/usuarios.py
+++ src/recursos/usuarios.py
@@ -7,8 +7,13 @@ usuario_model = api.model('Users', {
     'id': fields.Integer,
     'name': fields.String(attribute='nombre')
 })
+usuario_payload_model = api.model('Users', {
+    'name': fields.String(attribute='nombre'),
+    'password': fields.String(attribute='clave')
+})
 
 class Usuarios(Resource):
+    @api.expect(usuario_payload_model)
     @api.marshal_with(usuario_model, code = 201)
     def post(self):
         respuesta = usuario.insertar(api.payload["name"], api.payload["password"])
@@ -16,6 +21,7 @@ class Usuarios(Resource):
         return respuesta, 201
 
 class UsuarioLogin(Resource):
+    @api.expect(usuario_payload_model)
     def post(self):
         if usuario.login(api.payload["name"], api.payload["password"]):
             access_token = create_access_token(identity = api.payload["name"])
